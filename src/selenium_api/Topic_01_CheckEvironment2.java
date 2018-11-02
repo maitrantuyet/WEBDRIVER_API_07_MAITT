c246036539881469438b64c777697eb01ec245e6
N5537 <N5537@TuyetMai>
2018-11-02 21:57:33 +0700

    Update commit

---
+++ src/selenium_api/Topic_01_CheckEvironment2.java
@@ -0,0 +1,46 @@
+package selenium_api;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+public class Topic_01_CheckEvironment2 {
+	WebDriver driver;
+	
+ 
+  @BeforeClass
+  public void beforeClass() {
+	// Chrome
+			System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.get("https://www.google.com/");
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+				// Firefox
+				/*driver = new FirefoxDriver();
+				driver.get("https://www.google.com/");
+				driver.manage().window().maximize();
+				driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);*/
+  }
+  
+  @Test
+  public void TC_01_CheckTitle() {
+	  String homePageTitle = driver.getTitle();
+		Assert.assertEquals(homePageTitle, "Google");
+	}
+
+
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+
+}

35025a5fa16b7fbaff7ee7e5656d629f21983d23
N5537 <N5537@TuyetMai>
2018-11-20 23:08:11 +0700

    Topic_05_In Progress 2

--- src/selenium_api/Topic_05_ButtonRadiobuttonCheckboxAlert.java
+++ src/selenium_api/Topic_05_ButtonRadiobuttonCheckboxAlert.java
@@ -5,6 +5,7 @@ import org.testng.annotations.BeforeClass;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -42,41 +43,88 @@ public class Topic_05_ButtonRadiobuttonCheckboxAlert {
 	  Assert.assertEquals(driver.getCurrentUrl(), "http://live.guru99.com/index.php/customer/account/create/");
 	}
 
-  @Test
+  
   public void TC_02_RadioButton_HTML() {
 	 driver.get("https://daominhdam.github.io/basic-form/index.html"); 
+	 
+	 //Click Under18_radioButton and development_checkbox
+	 WebElement under18Radio = driver.findElement(By.xpath("//input[@id='under_18']"));
+	 WebElement developmentCheckbox = driver.findElement(By.xpath("//input[@id='development']"));
+	 under18Radio.click();
+	 developmentCheckbox.click();
+	 
+	 //Verify selected
+	 Assert.assertTrue(under18Radio.isSelected());
+	 Assert.assertTrue(developmentCheckbox.isSelected());
+	 
+	 
   }
-  @Test
+  
+  
   public void TC_03_HandleCheckbox_Custom() {
 	 driver.get("http://demos.telerik.com/kendo-ui/styling/checkboxes"); 
 	 //Click to 'Dual-zone air conditioning' checkbox
-	 WebElement dualZoneCheckbox = driver.findElement(By.xpath("//label[text()= 'Dual-zone air conditioning']"));
-	 dualZoneCheckbox.click();
-	 
+	 WebElement dualZoneCheckbox = driver.findElement(By.xpath("//label[text()= 'Dual-zone air conditioning']/preceding-sibling::input"));
+	 clickElementByJavascript(dualZoneCheckbox);
+
 	 //Verify duaZone Checkbox selected
 	 Assert.assertTrue(dualZoneCheckbox.isSelected());
 	 
 	 //Uncheck
-	 dualZoneCheckbox.click();
+	 clickElementByJavascript(dualZoneCheckbox);
 	 
 	//Verify duaZone Checkbox un-selected
 	Assert.assertFalse(dualZoneCheckbox.isSelected());
   }
   
-  @Test
+ 
   public void TC_04_HandleRadioButton() {
 	 driver.get("http://demos.telerik.com/kendo-ui/styling/radios"); 
 	 
+	 WebElement twoPetrolRadio = driver.findElement(By.xpath("//label[text() ='2.0 Petrol, 147kW']/preceding-sibling::input"));
+	 WebElement oneDotEightPetrolRadio = driver.findElement(By.xpath("//label[text() ='1.8 Petrol, 118kW']/preceding-sibling::input"));
+	 clickElementByJavascript(twoPetrolRadio);
+
+	 //Verify duaZone Checkbox selected
+	 Assert.assertTrue(twoPetrolRadio.isSelected());
+	 
+	 //Uncheck
+	 clickElementByJavascript(oneDotEightPetrolRadio);
+	 Assert.assertTrue(oneDotEightPetrolRadio.isSelected());
+	 
+	//Verify duaZone Checkbox un-selected
+	Assert.assertFalse(twoPetrolRadio.isSelected());
   }
 
+  @Test
+  public void TC_05_Alert() {
+	  driver.get("https://daominhdam.github.io/basic-form/index.html");
+	  driver.findElement(By.xpath("//button[text() ='Click for JS Alert']")).click();
+	  
+	  Alert alert = driver.switchTo().alert();
+	  Assert.assertEquals(alert.getText(), "I am a JS Alert");
+	  alert.accept();
+	  
+	  
+	  
+  }
+  
   @AfterClass
   public void afterClass() {
 	  driver.quit();
   }
   
+  // hàm này không quan tâm element có visible hay ko
   public void clickElementByJavascript(WebElement element) {
 	    JavascriptExecutor je = (JavascriptExecutor) driver;
 	    je.executeScript("arguments[0].click();", element);
+	    System.out.println("Click sucess!");
+	    try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }

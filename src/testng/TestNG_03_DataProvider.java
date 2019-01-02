23e28189054786b0fe6bd48cd42dbc1aad672ee9
Tuyet Mai Tran <maittt@vietpointer.vn>
2019-01-02 18:02:16 +0700

    Topic 11 TC03 In progress

--- src/testng/TestNG_03_DataProvider.java
+++ src/testng/TestNG_03_DataProvider.java
@@ -18,27 +18,40 @@ public class TestNG_03_DataProvider {
 	@BeforeClass
 	public void setupBrowser() {
 		driver = new FirefoxDriver();
-		driver.get("http://live.guru99.com/index.php/customer/account/login/");
+		driver.get("http://live.guru99.com/index.php/");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
 	
-	@Test(dataProvider = "dp")
-	public void TC_01_LoginToSystem() {
-		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("automationclass07@gmail.com");
-		driver.findElement(By.xpath("//*[@id='pass']")).sendKeys("123123");
+	@Test(dataProvider = "UserAndPassword")
+	public void TC_01_LoginToSystem(String email, String password) {
+		//Click to navigate to Login form
+		driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
+		
+		//Login
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys(email);
+		driver.findElement(By.xpath("//*[@id='pass']")).sendKeys(password);
 		driver.findElement(By.xpath("//*[@id='send2']")).click();
 		
+		//Verify User
 		Assert.assertTrue(driver.findElement(By.xpath("//h1[text()='My Dashboard']")).isDisplayed());
-
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@class= 'box-content']/p[contains(.,'" +email+ "')]")).isDisplayed());
+		
+		//Logout
+		driver.findElement(By.xpath("//span[text()='Account']")).click();
+		driver.findElement(By.xpath("//a[text()='Log Out']")).click();
+		
+		//Verify navigate to HomePage
+		Assert.assertTrue(driver.findElement(By.xpath("//h2[contains(text(),'This is demo site for')]")).isDisplayed());
 		
 	}
 
 	@DataProvider
-	public Object[][] dp() {
+	public Object[][] UserAndPassword() {
 		return new Object[][] {
-			new Object[] { 1, "a" },
-			new Object[] { 2, "b" },
+			{ "automationclass07@gmail.com", "123123" },
+			{ "automationclass08@gmail.com", "123123" },
+			{ "automationclass09@gmail.com", "123123" }
 		};
 	}
  
